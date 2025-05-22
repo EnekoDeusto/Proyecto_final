4a5f05c50479099cefa538ef4f3444975c904b60
Manel <manel.diaz@opendeusto.es>
2025-05-22 15:52:41 +0200

    cambios app.py

--- CAPTURADOR_PUBLICADOR/app.py
+++ CAPTURADOR_PUBLICADOR/app.py
@@ -4,16 +4,13 @@ import json
 import pandas as pd
 import paho.mqtt.client as mqtt
 
-# Parámetros de conexión
 MQTT_BROKER = os.environ.get("MQTT_BROKER", "localhost")
 MQTT_PORT = 1883
 MQTT_TOPIC = "calidad_aire/ciudades"
 
-# Leer el CSV
-CSV_PATH = "./Datos_calidad_aire/updated_pollution_dataset.csv"  # Asegúrate de que este archivo esté en Publicador/
+CSV_PATH = "./DATOS/global_air_pollution_data.csv"
 df = pd.read_csv(CSV_PATH)
 
-# Configurar cliente MQTT
 client = mqtt.Client()
 
 def main():
@@ -23,21 +20,16 @@ def main():
     print("Publicando datos...")
     for _, row in df.iterrows():
         payload = {
-            "Temperatura": row["Temperature"],
-            "Humedad": row["Humidity"],
-            "Pm2_5": row["PM2.5"],
-            "Pm10": row["PM10"],
-            "No2": row["NO2"],
-            "So2": row["SO2"],
-            "Co": row["CO"],
-            "Cercania_areas_industriales": row["Proximity_to_Industrial_Areas"],
-            "Populacion": row["Population_Density"],
-            "Calidad_Aire": row["Air Quality"]
+            "country_name": row["country_name"],
+            "city_name": row["city_name"],
+            "aqi_value": row["aqi_value.5"],
+            "aqi_category": row["aqi_category"],
+            "co_aqi_value": row["co_aqi_value"],
         }
 
         client.publish(MQTT_TOPIC, json.dumps(payload))
         print(f"Publicado: {payload}")
-        time.sleep(1)  # Esperar 1 segundo entre publicaciones
+        time.sleep(0.2)
 
     client.disconnect()
     print("Publicación finalizada.")
